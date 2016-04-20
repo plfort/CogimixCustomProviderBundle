29642a9fe793b48c845ca427c64adce20d6b524d
plfort <plfort@cogipix.com>
2016-04-20 18:56:00 +0200

    Posgtresql migration

--- Entity/CustomProviderInfo.php
+++ Entity/CustomProviderInfo.php
@@ -19,7 +19,7 @@ class CustomProviderInfo
     /**
      * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
